129f7a84877de14db52007ee5ec66775eff686ee
BeanRider <weng.jef@husky.neu.edu>
2016-05-20 22:44:45 -0400

    Moved custom javascript to app.js.

---
+++ public/assignment/assignment2/js/app.js
@@ -0,0 +1,12 @@
+var showingCard = true;
+$('.jw-card-toggle').change(function() {
+    showingCard = !showingCard;
+    if (showingCard) {
+        $('.jw-card-group').show();
+        $('.jw-list-group').hide();
+
+    } else {
+        $('.jw-card-group').hide();
+        $('.jw-list-group').show();
+    }
+});
