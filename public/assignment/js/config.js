e6405659f0cf7c5cfe7306bb8fffecd02514ec0b
BeanRider <weng.jef@husky.neu.edu>
2016-05-23 13:17:08 -0400

    Finished adding all routing configurations.

--- public/assignment/js/config.js
+++ public/assignment/js/config.js
@@ -21,6 +21,32 @@
             .when("/user/:uid", {
                 templateURL: "views/user/profile.view.client.html"
             })
+            .when("/user/:uid/website", {
+                templateURL: "views/website/website-list.view.client.html"
+            })
+            .when("/user/:uid/website/new", {
+                templateURL: "views/website/website-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid", {
+                templateURL: "views/website/website-edit.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page", {
+                templateURL: "views/page/page-list.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/new", {
+                templateURL: "views/page/page-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid", {
+                templateURL: "views/page/page-edit.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget", {
+                templateURL: "views/widget/widget-list.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/new", {
+                templateURL: "views/widget/widget-new.view.client.html"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
+                templateURL: "views/widget/widget-edit.view.client.html"
+            });
     }
-
-})
+});
