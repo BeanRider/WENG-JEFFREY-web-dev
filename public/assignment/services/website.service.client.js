1097589547d4bb40b48e471606deb5ccceea53dd
BeanRider <weng.jef@husky.neu.edu>
2016-05-25 19:42:19 -0400

    Finished website CRUD services

--- public/assignment/services/website.service.client.js
+++ public/assignment/services/website.service.client.js
@@ -14,18 +14,88 @@
         ]
 
         var api = {
+            createWebsite: createWebsite,
+            findWebsitesByUser: findWebsitesByUser,
+            findWebsiteById: findWebsiteById,
+            updateWebsite: updateWebsite,
+            deleteWebsite: deleteWebsite,
             findWebsitesForUser: findWebsitesForUser
         };
         return api;
 
-        function findWebsitesForUser(userId) {
-            var result = [];
+        /**
+         * C: Adds new website parameter instance to the local websites array
+         *    Sets new website's developerId to the given userId
+         * @param userId - the developer
+         * @param website - the website to add
+         */
+        function createWebsite(userId, website) {
+            websites.push({
+                "_id": website._id,
+                "name": website.name,
+                "developerId": userId
+            })
+        }
+
+        /**
+         * R: the websites with developerId === userId
+         * @param {number} userId
+         * @returns {Array} of matched websites
+         */
+        function findWebsitesByUser(userId) {
+            var matchedWebsites = [];
             for (var i in websites) {
                 if (websites[i].developerId === userId) {
-                    result.push(websites[i]);
+                    websites.push(angular.copy(websites[i]));
                 }
             }
-            return result;
+            return matchedWebsites;
+        }
+
+        /**
+         * R: the website with _id === given websiteId
+         * @param {number} websiteId
+         * @returns the matched website OR null if DNE
+         */
+        function findWebsiteById(websiteId) {
+            for (var i in websites) {
+                if (websites[i]._id === websiteId) {
+                    return websites[i];
+                }
+            }
+            return null;
+        }
+
+        /**
+         * U: Updates the users with _id === given websiteId with the given website
+         * @param {number} websiteId
+         * @param website to use to update
+         * @returns {boolean} true if update was successful
+         */
+        function updateWebsite(websiteId, website) {
+            for (var i in websites) {
+                if (websites[i]._id === websiteId) {
+                    websites[i].name = website.name;
+                    websites[i].developerId = website.developerId;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /**
+         * D: Deletes the website with _id === websiteId parameter
+         * @param {number} userId - user to delete
+         * @returns {boolean} true if the deletion was successful
+         */
+        function deleteWebsite(websiteId) {
+            for (var i in websites) {
+                if (websites[i]._id === websiteId) {
+                    websites.splice(i, 1);
+                    return true;
+                }
+            }
+            return false;
         }
     }
 })();
