0d3ab0c72416be2a26b1c4e15b267065f61ba2e7
BeanRider <weng.jef@husky.neu.edu>
2016-05-25 20:03:07 -0400

    Created and finished page services

---
+++ public/assignment/services/page.service.client.js
@@ -0,0 +1,104 @@
+(function() {
+    angular
+        .module("WAMApp")
+        .factory("PageService", PageService);
+
+    function PageService() {
+        var pages = [
+            { "_id": "321", "name": "Post 1", "websiteId": "456" },
+            { "_id": "432", "name": "Post 2", "websiteId": "456" },
+            { "_id": "543", "name": "Post 3", "websiteId": "456" }
+        ]
+
+        var api = {
+            createPage          : createPage,
+            findPageByWebsiteId : findPageByWebsiteId,
+            findPageById        : findPageById,
+            updatePage          : updatePage,
+            deletePage          : deletePage
+        }
+        return api;
+
+        /**
+         * C: Adds new page parameter instance to the local user array.
+         * @param {number} websiteId - the webpage id related to this new page
+         * @param page - the page to add
+         */
+        function createPage(websiteId, page) {
+            pages.push({
+                "_id": page._id,
+                "name": page.name,
+                "websiteId": websiteId
+            });
+        }
+
+        /**
+         * R: the pages with websiteId === givenWebsiteId
+         * @param {number} websiteId - the webpage id to match
+         * @return {array} array of pages with matching websiteId
+         */
+        function findPageByWebsiteId(websiteId) {
+            var matchedPages = [];
+            for (var i in pages) {
+                var pageI = pages[i]
+                if (pageI.websiteId === websiteId) {
+                    matchedPages.push(pageI);
+                }
+            }
+            return matchedPages;
+        }
+
+        /**
+         * R: the page with _id === pageId
+         * @param {number} pageId - the page id to match
+         * @return the matched page OR null if DNE
+         */
+        function findPageById(pageId) {
+            for (var i in pages) {
+                var pageI = pages[i];
+                if (pageI._id === pageId) {
+                    return pageI;
+                }
+            }
+            return null
+        }
+
+        /**
+         * U: Update the page with _id === pageId with the given page properties
+         * @param {number} pageId - the page id to match
+         * @param the page to be used to update
+         * @return {boolean} true if update was successful
+         *
+         */
+        function updatePage(pageId, page) {
+            for (var i in pages) {
+                var pageI = pages[i];
+                if (pageI._id === pageId) {
+                    pageI.name = page.name;
+                    pageI.developerId = page.developerId;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /**
+         * D: Deletes the user with _id === pageId
+         * @param {number} pageId - the page id to match
+         * @return {boolean} true if the deletion was successful
+         */
+        function deletePage(pageId) {
+            for (var i in pages) {
+                if (pages[i]._id === pageId) {
+                    pages.splice(i, 1);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+
+    }
+
+})()
